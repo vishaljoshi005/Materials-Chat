f00d90ed20ba7316029b95e9af824220df255834
Vishal Joshi <vishaljoshi005@gmail.com>
2019-02-18 21:10:21 +0530

    Rid of Version

--- app/src/main/java/in/ivishaljoshi/materialchats/Controller/MainActivity.kt
+++ app/src/main/java/in/ivishaljoshi/materialchats/Controller/MainActivity.kt
@@ -1,13 +1,10 @@
-package `in`.ivishaljoshi.materialchats
+package `in`.ivishaljoshi.materialchats.Controller
 
+import `in`.ivishaljoshi.materialchats.R
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
@@ -22,7 +19,9 @@ class MainActivity : AppCompatActivity() {
 
 
         val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+            this, drawer_layout, toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
         )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
